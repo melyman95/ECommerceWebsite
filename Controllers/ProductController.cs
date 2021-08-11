7f0f4ecce42c23598ef01f1e108c6eb4933770a1
melyman95 <mattlyman12@gmail.com>
2021-08-10 17:34:28 -0700

    added initial productcontext class with constructor and dependency injection

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿
+using ECommerceWebsite.data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext context;
+
+        public ProductController(ProductContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Displays view that lists all products
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            // Display all products
+            
+            return View();
+        }
+    }
+}
