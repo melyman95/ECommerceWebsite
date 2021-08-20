60970b0c4e9dc397846cc59d5296b465eae8a4c2
melyman95 <mattlyman12@gmail.com>
2021-08-20 01:06:54 -0700

    delete product implemented

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -81,6 +81,7 @@ namespace ECommerceWebsite.Controllers
             return View(p);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
             Product p =
@@ -89,6 +90,25 @@ namespace ECommerceWebsite.Controllers
                       select prod).SingleAsync();
 
             return View(p);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            Product p =
+               await (from prod in context.Products
+                      where prod.ProductId == id
+                      select prod).SingleAsync();
+
+            String prodTitle = p.Title;
+
+            context.Entry(p).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+
+            TempData["Message"] = prodTitle + " was deleted.";
+
+            return RedirectToAction("Index");
         }
     }
 }
