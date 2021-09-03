6a8e3f1b3fa977b93f2eb6657569a5f25d81b04c
melyman95 <mattlyman12@gmail.com>
2021-09-02 22:24:06 -0700

    added shopping cart controller

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceWebsite.data;
+using ECommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,25 @@ namespace ECommerceWebsite.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ProductContext context;
+
+        public CartController(ProductContext context)
         {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            // get product from database
+            // add product to cart cookie
+            // redirect back to previous page
+            //List<Product> products = await ProductDb.GetProductsAsync(context);
+            return View();
+        }
+
+        public IActionResult Summary()
+        {
+            // display all products in shopping cart cookie
             return View();
         }
     }
