cbe84c88e06460ca404e38aad52dc74f0f2bad8a
melyman95 <mattlyman12@gmail.com>
2021-09-02 22:59:10 -0700

    refactored get single product db query, and edit and delete product methods

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using ECommerceWebsite.data;
 using ECommerceWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,11 @@ namespace ECommerceWebsite.Controllers
         public async Task<IActionResult> AddToCart(int id)
         {
             // get product from database
+            await ProductDb.GetSingleProductAsync(context, id);
             // add product to cart cookie
+
             // redirect back to previous page
-            //List<Product> products = await ProductDb.GetProductsAsync(context);
+
             return View();
         }
 
