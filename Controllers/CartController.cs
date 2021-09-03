957281c4653f0560944b8557ee0cb3df8d31c7c0
melyman95 <mattlyman12@gmail.com>
2021-09-02 23:37:22 -0700

    able to add single product to shopping cart, redirects to product page for now

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,7 +1,9 @@
 ﻿using ECommerceWebsite.data;
 using ECommerceWebsite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,21 +14,29 @@ namespace ECommerceWebsite.Controllers
     public class CartController : Controller
     {
         private readonly ProductContext context;
+        private readonly IHttpContextAccessor httpContext;
 
-        public CartController(ProductContext context)
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
         {
             this.context = context;
+            this.httpContext = httpContext;
         }
 
         public async Task<IActionResult> AddToCart(int id)
         {
-            // get product from database
-            await ProductDb.GetSingleProductAsync(context, id);
-            // add product to cart cookie
+            Product p = await ProductDb.GetSingleProductAsync(context, id);
 
-            // redirect back to previous page
+            string data = JsonConvert.SerializeObject(p);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
 
-            return View();
+            httpContext.HttpContext.Response.Cookies.Append("Cart_Cookie", data, options);
+
+            return RedirectToAction("Index", "Product");
         }
 
         public IActionResult Summary()
